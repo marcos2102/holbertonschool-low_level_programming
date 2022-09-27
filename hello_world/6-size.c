d83e87915632accc4f260c590fca0d788680bf10
marcos2102 <5743@holbertonstudents.com>
2022-09-27 10:02:58 -0700

    sad

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu\n", sizeof(char), "%s byte(s)");
+	printf("Size of a char: %lu\n byte(s)", sizeof(char), "%s byte(s)");
 	printf("Size of an int: %lu\n", sizeof(int), " byte(s)");
 	printf("Size of a long int: %lu\n", sizeof(long int), " byte(s)");
 	printf("Size of a long long int: %lu\n", sizeof(long long int), " byte(s)");
