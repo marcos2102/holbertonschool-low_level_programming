3c412568a4ce45095f4c7ab356378de21228a2cf
marcos2102 <5743@holbertonstudents.com>
2022-09-28 16:24:12 -0700

    asd

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,3 +1,14 @@
+/*
+ * ********
+ ****   ***
+ **     ***
+ *  *   ***
+ ****   ***
+ ****   ***
+ ****   ***
+ ****   ***
+ ****   ***
+ */
 #include "main.h"
 /**
  * print_alphabet: print alphabete in lowercase
@@ -7,7 +18,7 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{	
+	{
 		_putchar(letter);
 	}
 	_putchar('\n');
