a1f538a8442079938ab2cf9562646fcda96a523e
marcos2102 <5743@holbertonstudents.com>
2022-09-29 07:51:07 -0700

    exc4 complete

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic characters, upper or lowercase.
+ *
+ * @c: the letter to be checked.
+ *
+ * Return: 1 if c is a letter, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	int rv; /*return value*/
+
+	if (c >= 65 && c <= 90)
+		|| (c >= 97 && c <= 122)
+	{
+		rv = 1;
+	} else
+	{
+		rv = 0;
+	}
+	return (rv);
+}
