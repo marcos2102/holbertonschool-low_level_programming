fe155afc3b4858927c7258ff8b51ff24124378c5
marcos2102 <5743@holbertonstudents.com>
2022-09-29 12:14:22 -0700

    asda

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers.
+ *
+ * @n1: integer 1 to add.
+ *
+ * @n2: integer 2 to add.
+ *
+ * Return: integer 1 (n1) plus integer 2 (n2).
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
