498c49ae009061828947c527a942629af9436c4b
marcos2102 <5743@holbertonstudents.com>
2022-09-29 10:18:36 -0700

    asd

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day, from 00:00 to 23:59.
+ *
+ * Return: Always 0 (Success).
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		_putchar(h1 + '0');
+		for (h2 = 0; h2 <= 3; h2++)
+		{
+			_putchar(h2 + '0');
+			_putchar(":");
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				_putchar(m1 + '0');
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					_putchar(m2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
