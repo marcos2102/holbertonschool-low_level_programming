1e248904d7b99f557057c234a636e6d878eeed11
marcos2102 <5743@holbertonstudents.com>
2022-09-29 12:42:18 -0700

    asd

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,30 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		printf("%d", i);
-		if (i != 98) 
+		for (i = n; i <= 98; i++)
 		{
-			printf(", ");
-		} else
-		{
-			printf("\n");
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			} else
+			{
+				printf("\n");
+			}
 		}
-	}
+	} else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			} else
+			{
+				printf("\n");
+			}
+		}
 }
