5b7a8ba48b832ef30f1df331e3f3c28bc341f98a
marcos2102 <5743@holbertonstudents.com>
2022-09-29 10:24:36 -0700

    ex8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,16 +9,16 @@ void jack_bauer(void)
 
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		_putchar(h1 + '0');
 		for (h2 = 0; h2 <= 3; h2++)
 		{
-			_putchar(h2 + '0');
-			_putchar(':');
 			for (m1 = 0; m1 <= 5; m1++)
 			{
-				_putchar(m1 + '0');
 				for (m2 = 0; m2 <= 9; m2++)
 				{
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
 					_putchar(m2 + '0');
 					_putchar('\n');
 				}
