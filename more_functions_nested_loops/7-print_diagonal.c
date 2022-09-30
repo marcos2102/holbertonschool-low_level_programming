98244551e1bd298a3a0397f40003174fc1f8c41c
marcos2102 <5743@holbertonstudents.com>
2022-09-30 16:34:25 -0700

    ex7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the termianl.
+ *
+ * @n: diagonal size (times '\' is printed).
+ */
+
+void print_diagonal(int n)
+{
+	int i; /*FOR index*/
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
