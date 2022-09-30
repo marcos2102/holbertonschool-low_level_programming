c0cc5a6c8dd1fbde0374062ddf17e4f6a602089e
marcos2102 <5743@holbertonstudents.com>
2022-09-30 16:11:55 -0700

    ex5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14.
+ */
+
+void more_numbers(void)
+{
+	int i, m; /*@i: FOR index
+			*@m: modulus
+			*/
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i / 10 != 0)
+		{
+			_putchar(i / 10 + '0');
+			m = i % 10;
+		} else
+		{
+			m = i;
+		}
+		_putchar(m + '0');
+	}
+}
