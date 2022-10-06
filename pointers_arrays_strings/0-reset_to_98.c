4aa0677dd2dee0718ffade648d2ec6eb865123ee
marcos2102 <5743@holbertonstudents.com>
2022-10-06 06:59:29 -0700

    name_fix

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of a variable to 98, using a pointer.
+ *
+ * @n: pointer to update value.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
