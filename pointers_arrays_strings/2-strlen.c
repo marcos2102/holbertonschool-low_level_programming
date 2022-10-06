22df3f138ef27f043e4a1ad5ae8510ea3de392da
marcos2102 <5743@holbertonstudents.com>
2022-10-06 07:11:34 -0700

    ex2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: string.
+ *
+ * Return: the @s length.
+ */
+
+int _strlen(char *s)
+{
+	int i, x; /*WHILE index*/
+
+	i = 0;
+	x = 0;
+	while (*s[i] != \0)
+	{
+		x++;
+		i++;
+	}
+	return (x);
+}
