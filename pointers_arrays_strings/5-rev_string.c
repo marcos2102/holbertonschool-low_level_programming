1802aafee9e0094f341d377b07e1965b2b61edca
marcos2102 <5743@holbertonstudents.com>
2022-10-06 13:07:34 -0700

    ex5_test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: string to reverse.
+ */
+
+void rev_string(char *s)
+{
+	char *aux;
+	int i, j, n;
+
+	i = 0;
+	n = 0;
+	while (s[i] != '\0')
+		i++;
+	for (j = i; j > 0; j--)
+		aux[n] = s[j-1];
+	*s = *n;
+}
