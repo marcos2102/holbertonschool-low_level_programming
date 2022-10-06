d9306d9d1849efbbcf9b767e2e05ed18351db7e9
marcos2102 <5743@holbertonstudents.com>
2022-10-06 13:12:55 -0700

    ex5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,12 @@ void rev_string(char *s)
 	char *aux;
 	int i, j, n;
 
+	aux = *s;
 	i = 0;
 	n = 0;
 	while (s[i] != '\0')
 		i++;
 	for (j = i; j > 0; j--)
-		*aux[n] = s[j - 1];
+		aux[n] = s[j - 1];
 	*s = n;
 }
