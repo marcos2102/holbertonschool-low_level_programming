9167f12be2872f661c999c80ffe91b9fe06a9b0a
marcos2102 <5743@holbertonstudents.com>
2022-10-06 14:52:05 -0700

    ex6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting with the first c.
+ *
+ * @str - string to print.
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+}
