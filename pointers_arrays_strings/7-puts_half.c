35f0fba4385407815b06d3fe82484cb277caad35
marcos2102 <5743@holbertonstudents.com>
2022-10-07 05:09:29 -0700

    ex7_test

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,11 +15,11 @@ void puts_half(char *str)
 	i--;
 	if (i % 2 == 0)
 	{
-		for (j = (i / 2); j <= i; j++)
+		for (j = (i / 2) + 1; j <= i; j++)
 			_putchar(str[j]);
 	} else
 	{
-		for (j = (i / 2) + 10; j <= i; j++)
+		for (j = (i / 2) + 1; j <= i; j++)
 			_putchar(str[j]);
 	}
 	_putchar('\n');
